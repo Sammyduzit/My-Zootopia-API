35ea650f7904bb6152452997efd9477f8dc1e445
Sammyduzit <sammy.duzit999@gmail.com>
2025-04-05 00:03:40 +0200

    added .gitignore and .env

--- Zootopia with API.py
+++ Zootopia with API.py
@@ -1,13 +1,21 @@
 import requests
 from pprint import pprint
+import os
+from dotenv import load_dotenv
 
-name = 'fox'
-api_url = 'https://api.api-ninjas.com/v1/animals?name={}'.format(name)
-response = requests.get(api_url, headers={'X-Api-Key': 'EvshZpIZFESeEJBXVPBVWA==q2RhOZnU0Uph2Pgf'})
-if response.status_code == requests.codes.ok:
-    json = response.json()
-    pprint(json)
-    print(response.status_code)
-else:
-    print("Error:", response.status_code, response.text)
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+
+
+def get_animal_from_api():
+    name = 'fox'
+    api_url = 'https://api.api-ninjas.com/v1/animals?name={}'.format(name)
+    response = requests.get(api_url, headers={'X-Api-Key': f'{API_KEY}'})
+    if response.status_code == requests.codes.ok:
+        json = response.json()
+        pprint(json)
+        print(response.status_code)
+    else:
+        print("Error:", response.status_code, response.text)
 
